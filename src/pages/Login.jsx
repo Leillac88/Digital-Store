5400c689808ce64dff8d2558de1a3b73cbc6e2a3
Leillac88 <leillacarvalho88@gmail.com>
2025-05-17 13:46:14 -0300

    add simple header

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,5 +3,5 @@ import Button from '../components/Button/Button.jsx';
 import tenisLogin from '../assets//tenisLogin.png';
 import gmailLogin from '../assets/gmailLogin.svg';
 import facebookLogin from '../assets/facebookLogin.svg';
-import SimpleHeader from '../components/SimpleHeader';
-import Input from '../components/Input';
+import SimpleHeader from '../components/SimpleHeader/SimpleHeader.jsx';
+import Input from '../components/Input/Input.jsx';
