3eca206adab8b8889dbbef9eea13de27070f2b5d
Leillac88 <leillacarvalho88@gmail.com>
2025-05-17 13:44:58 -0300

    add login imports

---
+++ src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import Footer from '../components/Footer/Footer.jsx';
+import Button from '../components/Button/Button.jsx';
+import tenisLogin from '../assets//tenisLogin.png';
+import gmailLogin from '../assets/gmailLogin.svg';
+import facebookLogin from '../assets/facebookLogin.svg';
+import SimpleHeader from '../components/SimpleHeader';
+import Input from '../components/Input';
